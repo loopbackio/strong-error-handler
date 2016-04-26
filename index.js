efff74b11b62b9b6d1c4fd8bb1310d5da1216cdd
Loay Gewily <loaygl@gmail.com>
2016-04-26 11:32:46 -0400

    add eslint and update code

--- index.js
+++ index.js
@@ -4,32 +4,27 @@
  * MIT Licensed
  */
 
-'use strict'
+'use strict';
 
 /**
  * Module dependencies.
  * @private
  */
 
-var accepts = require('accepts')
+var accepts = require('accepts');
 var escapeHtml = require('escape-html');
 var fs = require('fs');
-var util = require('util')
+var util = require('util');
 
 /**
  * Module variables.
  * @private
  */
 
-var doubleSpaceGlobalRegExp = /  /g
-var inspect = util.inspect
-var newLineGlobalRegExp = /\n/g
-var toString = Object.prototype.toString
-
-/* istanbul ignore next */
-var defer = typeof setImmediate === 'function'
-  ? setImmediate
-  : function(fn){ process.nextTick(fn.bind.apply(fn, arguments)) }
+var doubleSpaceGlobalRegExp = /  /g;
+var inspect = util.inspect;
+var newLineGlobalRegExp = /\n/g;
+var toString = Object.prototype.toString;
 
 /**
  * Strong Error handler:
@@ -57,107 +52,107 @@ var defer = typeof setImmediate === 'function'
  */
 exports = module.exports = function strongErrorHandler(options) {
   // get environment
-  var env = process.env.NODE_ENV || 'production'
+  var env = process.env.NODE_ENV || 'production';
   // enable the development mode?
   // In dev, all error properties (including) stack traces
   // are sent in the response
-  var debug = process.env.debug
-    ? console[ process.env.debug ]
-    : function() {}
+  var debug = process.env.debug ?
+    console[process.env.debug] :
+    function() {};
 
   // get options
-  var opts = options || {}
+  var opts = options || {};
 
   // get log option
-  var log = opts.log === true
-     ? env !== 'development'
-     : function() {}
+  var log = opts.log === true ?
+    env !== 'development' :
+    function() {};
 
   if (typeof log === 'function') {
     process.on('uncaughtException', function(err) {
-   delete err.stack;
-});
+      delete err.stack;
+    });
   }
 
   if (log === true) {
-    log = logerror
+    log = logerror;
   }
 
   if (typeof log !== 'function' && typeof log !== 'boolean') {
-    throw new TypeError('option log must be function or boolean')
+    throw new TypeError('option log must be function or boolean');
   }
 
   //var safeFields = options.safeFields;
 
-  return function strongErrorHandler(err, req, res, next){
+  return function strongErrorHandler(err, req, res, next) {
     // respect err.statusCode
     if (err.statusCode) {
-      res.statusCode = err.statusCode
+      res.statusCode = err.statusCode;
     }
 
     // respect err.status
     if (err.status) {
-      res.statusCode = err.status
+      res.statusCode = err.status;
     }
 
     // default status code to 500
     if (res.statusCode < 400) {
-      res.statusCode = 500
+      res.statusCode = 500;
     }
 
     // log the error
-    var str = stringify(err)
+    var str = stringify(err);
     if (log) {
-      defer(log, err, str, req, res)
+      defer(log, err, str, req, res);
     }
 
     // cannot actually respond
     if (res._header) {
-      return req.socket.destroy()
+      return req.socket.destroy();
     }
 
     // negotiate
-    var accept = accepts(req)
-    var type = accept.type('html', 'json', 'text')
+    var accept = accepts(req);
+    var type = accept.type('html', 'json', 'text');
 
     // Security header for content sniffing
-    res.setHeader('X-Content-Type-Options', 'nosniff')
+    res.setHeader('X-Content-Type-Options', 'nosniff');
 
     // html
     if (type === 'html') {
-      fs.readFile(__dirname + '/views/style.css', 'utf8', function(e, style){
+      fs.readFile(__dirname + '/views/style.css', 'utf8', function(e, style) {
         if (e) return next(e);
-        fs.readFile(__dirname + '/views/error.jade', 'utf8', function(e, html){
+        fs.readFile(__dirname + '/views/error.jade', 'utf8', function(e, html) {
           if (e) return next(e);
-          var isInspect = !err.stack && String(err) === toString.call(err)
-          var errorHtml = !isInspect
-            ? escapeHtmlBlock(str.split('\n', 1)[0] || 'Error')
-            : 'Error'
-          var stack = !isInspect
-            ? String(str).split('\n').slice(1)
-            : [str]
+          var isInspect = !err.stack && String(err) === toString.call(err);
+          var errorHtml = !isInspect ?
+            escapeHtmlBlock(str.split('\n', 1)[0] || 'Error') :
+            'Error';
+          var stack = !isInspect ?
+             String(str).split('\n').slice(1) :
+             [str];
           var stackHtml = stack
-            .map(function (v) { return '<li>' + escapeHtmlBlock(v) + '</li>' })
-            .join('')
+            .map(function(v) { return '<li>' + escapeHtmlBlock(v) + '</li>'; })
+            .join('');
           var body = html
             .replace('{style}', style)
             .replace('{stack}', stackHtml)
             .replace('{title}', escapeHtml(exports.title))
             .replace('{statusCode}', res.statusCode)
-            .replace(/\{error\}/g, errorHtml)
-          res.setHeader('Content-Type', 'text/html; charset=utf-8')
-          res.end(body)
+            .replace(/\{error\}/g, errorHtml);
+          res.setHeader('Content-Type', 'text/html; charset=utf-8');
+          res.end(body);
         });
       });
       if (res.statusCode === 401) {
-        fs.readFile(__dirname + '/views/error-unauthorized.html', 'utf8', function(e, html){
-            if (e) return next(e);
-          });
+        fs.readFile(__dirname + '/views/error-unauthorized.html', 'utf8', function(e, html) {
+          if (e) return next(e);
+        });
       }
       if (res.statusCode === 404)  {
-        fs.readFile(__dirname + '/views/error-not-found.jade', 'utf8', function(e, html){
-            if (e) return next(e);
-          });
+        fs.readFile(__dirname + '/views/error-not-found.jade', 'utf8', function(e, html) {
+          if (e) return next(e);
+        });
       }
     // json
     } else if (type === 'json') {
@@ -167,22 +162,22 @@ exports = module.exports = function strongErrorHandler(options) {
       }
       for (var prop in err) error[prop] = err[prop];
       var json = JSON.stringify({ error: error });
-      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.setHeader('Content-Type', 'application/json; charset=utf-8');
       res.end(json);
 // a custom JSON serializer function for producing JSON response bodies
 // @param sanitizedData: response data containing only safe properties
 // @param originalError: the original Error object
-    var  jsonSerializer = function(sanitizedData, originalError) {
+      var  jsonSerializer = function(sanitizedData, originalError) {
         if (originalError.name === 'ValidationError') {
           var details = sanitizedData.details || {};
           sanitizedData.issueCount =  details.codes && Object.keys(details.codes).length;
         }
-      return sanitizedData;
-    }
+        return sanitizedData;
+      };
     // plain text
     } else {
-      res.setHeader('Content-Type', 'text/plain; charset=utf-8')
-      res.end(str)
+      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.end(str);
     }
   };
 };
@@ -201,7 +196,7 @@ exports.title = 'Connect';
 function escapeHtmlBlock(str) {
   return escapeHtml(str)
   .replace(doubleSpaceGlobalRegExp, ' &nbsp;')
-  .replace(newLineGlobalRegExp, '<br>')
+  .replace(newLineGlobalRegExp, '<br>');
 }
 
 /**
@@ -210,17 +205,17 @@ function escapeHtmlBlock(str) {
  */
 
 function stringify(val) {
-  var stack = val.stack
+  var stack = val.stack;
 
   if (stack) {
-    return String(stack)
+    return String(stack);
   }
 
-  var str = String(val)
+  var str = String(val);
 
-  return str === toString.call(val)
-    ? inspect(val)
-    : str
+  return str === toString.call(val) ?
+     inspect(val) :
+     str;
 }
 
 /**
@@ -229,5 +224,5 @@ function stringify(val) {
  */
 
 function logerror(err, str) {
-  console.error(str)
+  console.error(str);
 }
---
+++ test/index.test.js
@@ -0,0 +1,434 @@
+
+process.env.NODE_ENV = 'test';
+
+var after = require('after');
+var assert = require('assert');
+var strongErrorHandler = require('..');
+var http = require('http');
+var request = require('supertest');
+var util = require('util');
+var loopback = require('loopback');
+
+describe('strongErrorHandler()', function() {
+  it('should set nosniff header', function(done) {
+    var server = createServer(new Error('boom!'));
+    request(server)
+    .get('/')
+    .expect('X-Content-Type-Options', 'nosniff')
+    .expect(500, done);
+  });
+
+  describe('status code', function() {
+    describe('when non-error status code', function() {
+      it('should set the status code to 500', function(done) {
+        var server = createServer({ status: 200 });
+        request(server)
+        .get('/')
+        .expect(500, done);
+      });
+    });
+
+    describe('when err.status exists', function() {
+      it('should set res.statusCode', function(done) {
+        var server = createServer({ status: 404 });
+        request(server)
+        .get('/')
+        .expect(404, done);
+      });
+    });
+
+    describe('when err.statusCode exists', function() {
+      it('should set res.statusCode', function(done) {
+        var server = createServer({ statusCode: 404 });
+        request(server)
+        .get('/')
+        .expect(404, done);
+      });
+    });
+
+    describe('when err.statusCode and err.status exist', function() {
+      it('should prefer err.status', function(done) {
+        var server = createServer({ statusCode: 400, status: 404 });
+        request(server)
+        .get('/')
+        .expect(404, done);
+      });
+    });
+  });
+
+  describe('error value', function() {
+    describe('when Error object', function() {
+      it('should use "stack" property', function(done) {
+        var error = new TypeError('boom!');
+        var server = createServer(error);
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect(500, error.stack.toString(), done);
+      });
+    });
+
+    describe('when string', function() {
+      it('should pass-through string', function(done) {
+        var server = createServer('boom!');
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect(500, 'boom!', done);
+      });
+    });
+
+    describe('when number', function() {
+      it('should stringify number', function(done) {
+        var server = createServer(42.1);
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect(500, '42.1', done);
+      });
+    });
+
+    describe('when object', function() {
+      it('should use util.inspect', function(done) {
+        var server = createServer({ hop: 'pop' });
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect(500, '{ hop: \'pop\' }', done);
+      });
+    });
+
+    describe('with "toString" property', function() {
+      it('should use "toString" value', function(done) {
+        var server = createServer({ toString: function() { return 'boom!'; } });
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect(500, 'boom!', done);
+      });
+    });
+  });
+
+  describe('response content type', function() {
+    var error;
+    var server;
+
+    before(function() {
+      error = new Error('boom!');
+      error.description = 'it went this way';
+      server = createServer(error);
+    });
+
+    describe('when "Accept: text/html"', function() {
+      it('should return a html response', function(done) {
+        request(server)
+        .get('/')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /text\/html/)
+        .expect(/<title>Error: boom!<\/title>/)
+        .expect(/<h2><em>500<\/em> Error: boom!<\/h2>/)
+        .expect(/<li> &nbsp; &nbsp;at/)
+        .expect(500, done);
+      });
+
+      it('should contain inspected object', function(done) {
+        request(createServer({ foo: 'bar', fizz: 'buzz' }))
+        .get('/')
+        .set('Accept', 'text/html')
+        .expect('Content-Type', /text\/html/)
+        .expect(/<title>Error<\/title>/)
+        .expect(/<h2><em>500<\/em> Error<\/h2>/)
+        .expect(/<li>{ foo: &#39;bar&#39;, fizz: &#39;buzz&#39; }<\/li>/)
+        .expect(500, done);
+      });
+    });
+
+    describe('when "Accept: application/json"', function() {
+      it('should return a json response', function(done) {
+        var body = {
+          error: {
+            message: 'boom!',
+            description: 'it went this way',
+            stack: error.stack.toString(),
+          },
+        };
+
+        request(server)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /application\/json/)
+        .expect(500, body, done);
+      });
+    });
+
+    describe('when "Accept: text/plain"', function() {
+      it('should return a plain text response', function(done) {
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect('Content-Type', /text\/plain/)
+        .expect(500, error.stack.toString(), done);
+      });
+    });
+
+    describe('when "Accept: x-unknown"', function() {
+      it('should return a plain text response', function(done) {
+        request(server)
+        .get('/')
+        .set('Accept', 'x-unknown')
+        .expect('Content-Type', /text\/plain/)
+        .expect(500, error.stack.toString(), done);
+      });
+    });
+  });
+
+  describe('headers sent', function() {
+    var server;
+
+    before(function() {
+      var _errorHandler = strongErrorHandler();
+      server = http.createServer(function(req, res) {
+        res.end('0');
+        process.nextTick(function() {
+          _errorHandler(new Error('boom!'), req, res, function(error) {
+            process.nextTick(function() {
+              throw error;
+            });
+          });
+        });
+      });
+    });
+
+    it('should not die', function(done) {
+      request(server)
+      .get('/')
+      .expect(200, done);
+    });
+  });
+
+  describe('console', function() {
+    var _consoleerror;
+
+    before(function() {
+      _consoleerror = console.error;
+      process.env.NODE_ENV = '';
+    });
+    afterEach(function() {
+      console.error = _consoleerror;
+      process.env.NODE_ENV = 'test';
+    });
+
+    it('should output error', function(done) {
+      var cb = after(2, done);
+      var error = new Error('boom!');
+      var server = createServer(error);
+
+      console.error = function() {
+        var log = util.format.apply(null, arguments);
+
+        if (log !== error.stack.toString()) {
+          return _consoleerror.apply(this, arguments);
+        }
+
+        cb();
+      };
+
+      request(server)
+      .get('/')
+      .set('Accept', 'text/plain')
+      .expect(500, error.stack.toString(), done);
+    });
+  });
+});
+
+describe('strongErrorHandler(options)', function() {
+  describe('log', function() {
+    it('should reject a string', function() {
+      assert.throws(strongErrorHandler.bind(null, { log: 'yes, please' }), /option log must be/);
+    });
+
+    describe('when "undefined"', function() {
+      var _consoleerror;
+
+      before(function() {
+        _consoleerror = console.error;
+      });
+      afterEach(function() {
+        console.error = _consoleerror;
+      });
+
+      describe('when NODE_ENV == test', function() {
+        alterEnvironment('NODE_ENV', 'test');
+
+        it('should produce no output', function(done) {
+          var error = new Error('boom!');
+          var server = createServer(error);
+
+          console.error = function() {
+            var log = util.format.apply(null, arguments);
+
+            if (log !== error.stack.toString()) {
+              return _consoleerror.apply(this, arguments);
+            }
+
+            done(new Error('console.error written to'));
+          };
+
+          request(server)
+          .get('/')
+          .set('Accept', 'text/plain')
+          .expect(500, error.stack.toString(), done);
+        });
+      });
+
+      describe('when NODE_ENV != test', function() {
+        alterEnvironment('NODE_ENV', '');
+
+        it('should write to console', function(done) {
+          var cb = after(2, done);
+          var error = new Error('boom!');
+          var server = createServer(error);
+
+          console.error = function() {
+            var log = util.format.apply(null, arguments);
+
+            if (log !== error.stack.toString()) {
+              return _consoleerror.apply(this, arguments);
+            }
+
+            cb();
+          };
+
+          request(server)
+          .get('/')
+          .set('Accept', 'text/plain')
+          .expect(500, error.stack.toString(), cb);
+        });
+      });
+    });
+
+    describe('when "true"', function() {
+      var _consoleerror;
+
+      before(function() {
+        _consoleerror = console.error;
+      });
+      afterEach(function() {
+        console.error = _consoleerror;
+      });
+
+      it('should write to console', function(done) {
+        var cb = after(2, done);
+        var error = new Error('boom!');
+        var server = createServer(error, { log: true });
+
+        console.error = function() {
+          var log = util.format.apply(null, arguments);
+
+          if (log !== error.stack.toString()) {
+            return _consoleerror.apply(this, arguments);
+          }
+
+          cb();
+        };
+
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect(500, error.stack.toString(), cb);
+      });
+    });
+
+    describe('when "false"', function() {
+      var _consoleerror;
+
+      alterEnvironment('NODE_ENV', '');
+      before(function() {
+        _consoleerror = console.error;
+      });
+      afterEach(function() {
+        console.error = _consoleerror;
+      });
+
+      it('should not write to console', function(done) {
+        var error = new Error('boom!');
+        var server = createServer(error, { log: false });
+
+        console.error = function() {
+          var log = util.format.apply(null, arguments);
+
+          if (log !== error.stack.toString()) {
+            return _consoleerror.apply(this, arguments);
+          }
+
+          done(new Error('console.error written to'));
+        };
+
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect(500, error.stack.toString(), done);
+      });
+
+      it('should pass options on to error handler module', function(done) {
+     //arrange
+        var app = loopback();
+        app.use(loopback.urlNotFound());
+        app.use(loopback.errorHandler({ includeStack: false, log: customLogger }));
+
+     //act
+        request(app).get('/url-does-not-exist').end();
+
+     //assert
+        function customLogger(err, str, req) {
+          assert.ok(err.message === 'Cannot GET /url-does-not-exist');
+          done();
+        }
+      });
+    });
+
+    describe('when a function', function() {
+      it('should call function', function(done) {
+        var cb = after(2, done);
+        var error = new Error('boom!');
+        var server = createServer(error, { log: log });
+
+        function log(err, str, req, res) {
+          assert.equal(err, error);
+          assert.equal(str, error.stack.toString());
+          assert.equal(req.url, '/');
+          assert.equal(res.statusCode, 500);
+          cb();
+        }
+
+        request(server)
+        .get('/')
+        .set('Accept', 'text/plain')
+        .expect(500, error.stack.toString(), cb);
+      });
+    });
+  });
+});
+
+function createServer(error, options) {
+  var _errorHandler = strongErrorHandler(options);
+
+  return http.createServer(function(req, res) {
+    _errorHandler(error, req, res, function(err) {
+      res.statusCode = err ? 500 : 404;
+      res.end(err ? 'Critical: ' + err.stack : 'oops');
+    });
+  });
+}
+
+function alterEnvironment(key, value) {
+  var prev;
+
+  before(function() {
+    prev = process.env[key];
+    process.env[key] = value;
+  });
+  after(function() {
+    process.env[key] = prev;
+  });
+}
